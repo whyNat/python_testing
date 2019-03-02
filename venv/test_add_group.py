815ede3e229907ee5b2a2487963591233afd2328
WAFEL\Dom <nwaszak@wp.pl>
2019-03-02 18:07:17 +0100

    changes for driver

--- venv/test_add_group.py
+++ venv/test_add_group.py
@@ -12,6 +12,7 @@ class TestAddGroup(unittest.TestCase):
         wd.get("http://localhost/addressbook")
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("%s" % username)
         wd.find_element_by_name("pass").clear()
@@ -22,6 +23,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -35,6 +37,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys("%s" % group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
 
     def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
@@ -44,20 +47,14 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="bodyguard", header="logo", footer="takietam"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def tearDown(self):
