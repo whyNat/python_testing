2dade8fb515be7c24089f4888d8f734f416e5996
WAFEL\Dom <nwaszak@wp.pl>
2019-03-02 13:34:05 +0100

    methods and classes for adding contacts

---
+++ venv/contact.py
@@ -0,0 +1,14 @@
+
+class Contact:
+    def __init__(self, contactname, contactsurname, contactnickname, title, company, address, homephone,
+                       mobilephone, email, byear):
+        self.contactname = contactname
+        self.contactsurname = contactsurname
+        self.contactnickname = contactnickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.email = email
+        self.byear = byear
