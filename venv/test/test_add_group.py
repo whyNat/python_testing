29d9026c84da4cdac0c6b1a24794b40cb808ae0d
WAFEL\Dom <nwaszak@wp.pl>
2019-03-03 15:53:56 +0100

    isolated classess

--- venv/test/test_add_group.py
+++ venv/test/test_add_group.py
@@ -12,10 +12,10 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="bodyguard", header="logo", footer="takietam"))
+    app.group.create(Group(name="bodyguard", header="logo", footer="takietam"))
     app.session.logout()
 
 def test_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
