6f90167faead81f8cba8b259377b9e08eeba733c
WAFEL\Dom <nwaszak@wp.pl>
2019-03-28 16:49:53 +0100

    list comparison and sort

--- venv/geom2d/point.py
+++ venv/geom2d/point.py
@@ -12,5 +12,14 @@ class Point:
         dy = p2.y - p1.y
         return sqrt(dx*dx + dy * dy)
 
+    # operacja używana do porównania
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
+
+    # operacja używana do sortowania
+    def __lt__(self, other):
+        return self.y < other.y
+
+    # czytelne wyświetlanie na konsoli list
+    def __repr__(self):
+        return 'Point(%s, %s)' %(self.x, self.y)
