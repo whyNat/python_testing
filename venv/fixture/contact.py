e41d4f5028693d7029eacb37fdec7b031a8a298b
WAFEL\Dom <nwaszak@wp.pl>
2019-04-03 20:57:04 +0200

    cannot assert phones from view and edit page

--- venv/fixture/contact.py
+++ venv/fixture/contact.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.support.select import Select
-
 from model.contact import Contact
+import re
 
 
 class ContactHelper:
@@ -10,7 +10,7 @@ class ContactHelper:
     def check_if_contacts_page(self):
         wd = self.app.wd
         if not (len(wd.find_elements_by_link_text("Logout")) > 0 and len(wd.find_elements_by_name("searchstring")) > 0):
-            wd.find_element_by_link_text("home page").click()
+            wd.find_element_by_link_text("home").click()
 
     def create(self, contact):
         wd = self.app.wd
@@ -116,8 +116,8 @@ class ContactHelper:
                 lastname = cells[1].text
                 firstname = cells[2].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
-                all_phones = cells[5].text.splitlines()
-                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id, homephone=all_phones[0], workphone=all_phones[1]))        #, mobilephone=all_phones[2], secondaryphone=all_phones[3]
+                all_phones = cells[5].text
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id, all_phones_from_home_page=all_phones))
         return list(self.contact_cache)
 
     def open_contact_to_edit_by_index(self, index):
@@ -146,6 +146,35 @@ class ContactHelper:
         secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
         return Contact(firstname=firstname, lastname=lastname, id=id,
                        homephone=homephone, workphone=workphone, mobilephone=mobilephone, secondaryphone=secondaryphone)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        homephone = re.search("H: (.*)", text)
+        if homephone:
+            homephone = homephone.group(1)
+
+        mobilephone = re.search("M: (.*)", text)
+        if mobilephone:
+            mobilephone = mobilephone.group(1)
+
+        workphone = re.search("W: (.*)", text)
+        if workphone:
+            workphone = workphone.group(1)
+
+        secondaryphone = re.search("P: (.*)", text)
+        if secondaryphone:
+            secondaryphone = secondaryphone.group(1)
+
+        # homephone = re.search("H: (.*)", text).group(1)
+        # mobilephone = re.search("M: (.*)", text).group(1)
+        # workphone = re.search("W: (.*)", text).group(1)
+        # secondaryphone = re.search("P: (.*)", text).group(1)
+
+        return Contact(homephone=homephone, workphone=workphone, mobilephone=mobilephone, secondaryphone=secondaryphone)
+
+
 
 
 
--- venv/model/contact.py
+++ venv/model/contact.py
@@ -2,26 +2,30 @@ from sys import maxsize
 
 
 class Contact:
-    def __init__(self, contactname=None, contactsurname=None, contactnickname=None, title=None, company=None, address=None, homephone=None,
-                       mobilephone=None, email=None, month = None, byear=None, id=None):
-        self.contactname = contactname
-        self.contactsurname = contactsurname
-        self.contactnickname = contactnickname
+    def __init__(self, firstname=None, lastname=None, id=None, nickname=None, title=None, company=None, address=None, homephone=None,
+                 mobilephone=None, workphone=None, secondaryphone=None, email=None, month = None, byear=None, all_phones_from_home_page=None):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.nickname = nickname
         self.title = title
         self.company = company
         self.address = address
         self.homephone = homephone
         self.mobilephone = mobilephone
+        self.workphone = workphone
+        self.secondaryphone = secondaryphone
         self.email = email
         self.month = month
         self.byear = byear
         self.id = id
+        self.all_phones_from_home_page = all_phones_from_home_page
 
     def __repr__(self):
-        return '%s:%s' % (self.id, self.contactsurname)   # representation - porównuje wg fizycznego rozmieszczenia obiektów w pamięci
+        return '%s:%s' % (self.id, self.lastname)   # representation - porównuje wg fizycznego rozmieszczenia obiektów w pamięci
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.contactsurname == other.contactsurname      # porównuje po nazwie i id
+        return (self.id is None or other.id is None or self.id == other.id) \
+               and self.lastname == other.lastname and self.firstname == other.firstname     # porównuje po nazwie i id
 
     def id_or_max(self):
         if self.id:
