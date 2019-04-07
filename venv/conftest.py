eda207dc675b9ec87cee92ce85feeda01c2a4313
WAFEL\Dom <nwaszak@wp.pl>
2019-04-07 14:53:48 +0200

    configuration file

--- venv/conftest.py
+++ venv/conftest.py
@@ -1,21 +1,26 @@
+import json
+import os.path
 import pytest
 from fixture.application import Application
 
 
 fixture = None
+target = None
 
 
 @pytest.fixture                     # żeby przeglądarka otwierała się raz na całą sesję można dodać (scope='session')
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption(
+            "--target"))  # zawiera info o lokalizacji bierzacego pliku, lokalizacja absolutna
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target["baseUrl"])
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 
@@ -29,5 +34,5 @@ def stop(request):
 # wybor przegladarki z linii komend:
 def pytest_addoption(parser):                   # dodaje dodatkowe parametry przy uruchomieniu z wiersza polece
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook")
+    parser.addoption("--target", action="store", default="target.json")
 
