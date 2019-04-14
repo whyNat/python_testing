2fffb6c36a1ba95e94555d89244a9184d958fdba
WAFEL\Dom <nwaszak@wp.pl>
2019-04-14 21:27:38 +0200

    database driver

---
+++ check_db_connection.py
@@ -0,0 +1,13 @@
+#import mysql.connector
+import pymysql.cursors
+
+
+connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+
+try:
+    cursor = connection.cursor()
+    cursor.execute("select * from group_list")
+    for row in cursor.fetchall():
+        print(row)
+finally:
+    connection.close()
